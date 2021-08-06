c909aab354502f28bbd8ab19d1f1c1956629ea3d
noticablepig <39356292+noticablepig@users.noreply.github.com>
2021-08-06 13:15:48 -0500

    Add files via upload

---
+++ tuple-structs/target/debug/deps/tuple_structs.d
@@ -0,0 +1,5 @@
+D:\Projects\!Learning Rust\tuple-structs\target\debug\deps\tuple_structs.exe: src\main.rs
+
+D:\Projects\!Learning Rust\tuple-structs\target\debug\deps\tuple_structs.d: src\main.rs
+
+src\main.rs:
